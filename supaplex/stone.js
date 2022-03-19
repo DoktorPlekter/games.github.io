9ceb8975ff0cce98aad20bec5c6a63bed65e0c81
Doktor Plekter <DoktorPlekter@outlook.com>
2022-03-19 14:53:13 +0100

    supap update

---
+++ supaplex/stone.js
@@ -0,0 +1,14 @@
+
+
+function stone() {
+
+  this.draw = function(){
+    noStroke(); 
+  fill (25,155,55)
+  rectMode(CENTER)
+  rect(300,300,25)
+    
+  }
+  
+}
+
--- supaplex/supaplexsketch.js
+++ supaplex/supaplexsketch.js
@@ -8,10 +8,9 @@ function draw() {
   background(25);
   noStroke(); 
   fill (250,0,0)
-    circle(200,200,25)
+  circle(200,200,25);
+  stone[1].draw(); 
 
-    noStroke(); 
-    fill (25,155,55)
-    rectMode(CENTER)
-      rect(300,300,25)
+
+    
 }
