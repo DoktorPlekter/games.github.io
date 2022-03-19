ac168d485c3418598f236668d53886fa9eeb419f
Doktor Plekter <DoktorPlekter@outlook.com>
2022-03-19 11:58:27 +0100

    supaup1

---
+++ sketchnummerto.js
@@ -0,0 +1,17 @@
+
+
+function setup() {
+  createCanvas(512, 512);
+}
+
+function draw() {
+  background(25);
+  noStroke(); 
+  fill (250,0,0)
+    circle(200,200,25)
+
+    noStroke(); 
+    fill (25,155,55)
+    rectMode(CENTER)
+      rect(300,300,25)
+}
