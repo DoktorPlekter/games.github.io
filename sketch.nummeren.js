2e087f6ae237b24576ab5231ae3c351d840b58f1
Doktor Plekter <DoktorPlekter@outlook.com>
2022-03-19 11:23:56 +0100

    upd319

---
+++ sketch.nummeren.js
@@ -0,0 +1,13 @@
+
+function preload() {
+
+}
+
+function setup() {
+  createCanvas(512, 512);
+}
+
+function draw() {
+  background(25);
+    rect(200,200,25,25)
+}
---
+++ sketch.nummerto.js
@@ -0,0 +1,14 @@
+
+function preload() {
+
+}
+
+function setup() {
+  createCanvas(512, 512);
+}
+
+function draw() {
+  background(25);
+  fill(199, 55, 133)
+    rect(200,200,255,155,12)
+}
