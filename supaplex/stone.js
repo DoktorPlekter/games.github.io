d9abeb99bb046c6705973b6c73aae674a9ec519e
Doktor Plekter <DoktorPlekter@outlook.com>
2022-03-19 17:51:12 +0100

    supap

--- supaplex/stone.js
+++ supaplex/stone.js
@@ -8,15 +8,15 @@ function Stone(x, y) {
 
   this.display = function()
   {
+  
   noStroke(); 
   fill (75);
   circle(x,y,25);
   }
   
 
-  this.move = function() { 
-    this.x = this.x + random(-1, 1); 
-    this.y = this.y + random(-1, 1 );
+  this.place = function() { 
+
   }
 
 }
--- supaplex/supaplexsketch.js
+++ supaplex/supaplexsketch.js
@@ -4,23 +4,30 @@ var charmap =['g', 'g', 'g', 'g', 's', 'g', 'g', 'g', 's', 's']
 
 function setup() {
   createCanvas(512, 512);
+  
+  
+  charmap.forEach((char, ind = 0) => {
 
+    switch(char){
+      case 's':  
+        console.log('g was met')
+        var s =  new Stone(ind * 50, 100);
+        Stones.push(s);
+      break;
+      case 'g':  
+        console.log('g was met')
+        var s =  new Stone(ind * 50, 100);
+        Stones.push(s);
+      break;
+      default: console.log('Nothing met')
+    }
+   
+  
+  })
 }
 
 
-charmap.forEach((char, ind = 0) => {
 
-  switch(char){
-    case 'g':  
-    console.log('g was met')
-    //Stones.push(new Stone(100, 100));
-    break;
-
-    default: console.log('Nothing met')
-  }
- 
-
-})
 
 
 function draw() {
@@ -29,7 +36,7 @@ function draw() {
   fill (250,0,0)
   circle(200,200,25);
 
-  for (var i = 0; i < 0; i++ ) {
+  for (var i = 0; i < charmap.length; i++ ) {
     Stones[i].display();
 
   }
